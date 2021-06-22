35f646ea2d30d643a5ec708e7314170f3e6c73f5
kirkirey <55382480+kirkirey@users.noreply.github.com>
2021-06-22 03:27:33 +0300

    Update calculator.py

--- algorithms/calculator/calculator.py
+++ algorithms/calculator/calculator.py
@@ -1,9 +1,9 @@
 """
 Programming for linguists
-
 Implementation of the Reverse Polish Notation Converter
 """
-from algorithms.calculator.reverse_polish_notation import ReversePolishNotation
+
+from algorithms.calculator.reverse_polish_notation import ReversePolishNotation, Digit, Op
 from data_structures.stack.stack import Stack
 
 
@@ -18,7 +18,20 @@ class ReversePolishNotationCalculator:
         """
         Main method of the ReversePolishNotationCalculator class.
         Calculating result of expression in Reverse Polish Notation.
-
         :param rpn_expression: expression in Reverse Polish Notation Format
         :return: result of the expression
         """
+        for el in rpn_expression:
+            if isinstance(el, Digit):
+                self.stack.push(el)
+                right = self.stack.top()
+                self.stack.pop()
+                left = self.stack.top()
+                self.stack.pop()
+                res = el(left, right)
+                self.stack.push(res)
+                final = self.stack.top()
+                self.stack.pop()
+            else:
+                final = 0
+        return final
