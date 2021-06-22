97b8ec60044041a7dbe873adde048fff5d825612
kirkirey <55382480+kirkirey@users.noreply.github.com>
2021-06-22 03:29:01 +0300

    Update bracket.py

--- algorithms/calculator/reverse_polish_notation/bracket.py
+++ algorithms/calculator/reverse_polish_notation/bracket.py
@@ -1,24 +1,24 @@
 """
 Programming for linguists
-
 Interfaces for brackets
 """
-from algorithms.calculator.reverse_polish_notation.op import Op
 
+from algorithms.calculator.reverse_polish_notation.op import Op
 
 class Bracket(Op):
     """
     Base interface for brackets
     """
-
-
+    
 class OpenBracket(Bracket):
     """
     Interface for open bracket
     """
+    symbol = '('
 
 
 class CloseBracket(Bracket):
     """
     Interface for close bracket
     """
+    symbol = ')'
