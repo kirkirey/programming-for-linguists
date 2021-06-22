990fb34c1858b58577f716618db32748af1d3e47
kirkirey <55382480+kirkirey@users.noreply.github.com>
2021-06-22 03:29:28 +0300

    Update digit.py

--- algorithms/calculator/reverse_polish_notation/digit.py
+++ algorithms/calculator/reverse_polish_notation/digit.py
@@ -1,10 +1,9 @@
 """
 Programming for linguists
-
 Interfaces for digits
 """
-from typing import Union
 
+from typing import Union
 from algorithms.calculator.reverse_polish_notation.element import Element
 
 
@@ -12,17 +11,19 @@ class Digit(Element):
     """
     Interface for presenting a digit
     """
-
+    
     def __init__(self, digit_as_string: Union[float, str]):
         print(digit_as_string)
         self.digit = None
-
+        
     def __str__(self) -> str:
         """
         Magic method to present an instance of this class as string
         """
+        return str(self.digit)
 
     def __eq__(self, other: 'Digit') -> bool:
         """
         Magic method to compare instances of Digit class
         """
+        return self.digit == other.digit
